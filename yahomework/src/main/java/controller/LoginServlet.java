b11d5be8c34c68bcb049d883950733e1ba0c9f7a
Kuanwei <Kuanwei@DESKTOP-Q3LU8R3>
2022-12-19 18:30:41 +0800

    servlet setencoding utf-8

--- yahomework/src/main/java/controller/LoginServlet.java
+++ yahomework/src/main/java/controller/LoginServlet.java
@@ -25,8 +25,10 @@ public class LoginServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		
+		String username=request.getParameter("username");
+		String password=request.getParameter("password");
 		
-		System.out.println("test");
+		System.out.printf("username=%s, password=%s\n",username,password);
 		
 		doGet(request, response);
 	}
